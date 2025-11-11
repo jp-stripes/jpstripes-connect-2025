67f45af22ed0dca87f3fb4d96f2045714b8f90f4
Hidetaka Okamoto <kokkoku214@gmail.com>
2025-11-11 11:26:02 +0900

    update speakers

--- src/app/components/Speakers.tsx
+++ src/app/components/Speakers.tsx
@@ -2,14 +2,19 @@ import Image from 'next/image';
 
 // スピーカーの画像リスト
 const speakers = [
-    { image: '/speakers/1516767400750.jpg', name: 'Stripe Daniel Heffernanさん' },
-    { image: '/speakers/b5f641fdf20fc10651b5c8797b6e6461.png', name: '株式会社Helpfeel 秋山さん' },
-    { image: '/speakers/1746562415085.jpg', name: 'Studio, Inc 八木さん' },
-  { image: '/speakers/12a26cb6aa9617aed86cbb8ea40e5f2b.jpg', name: 'アトモフ株式会社中野 恭兵さん' },
-  { image: '/speakers/3d64468036802b636c3b2b10623099c3.jpg', name: '株式会社 TBSテレビ 亀田 遼さん' },
-  { image: '/speakers/a3de51c09d6885bd730934cba9e69eab.jpg', name: '株式会社favy森さん' },
-  { image: '/speakers/e279e71ea43f36f207e3508ee66b83ba.jpg', name: '株式会社DIGITALJET 真崎 克宏さん' },
-  { image: '/speakers/DSC_5282.JPG', name: 'Revtrona 岡本さん' },
+  { image: '/speakers/1516767400750.jpg', company: 'Stripe', name: 'Daniel Heffernanさん' },
+  { image: '', company: 'Stripe プロフェッショナルサービスチーム', name: '齊藤 光正さん' },
+  { image: '', company: '株式会社Still Day One', name: '小島 英揮さん' },
+  { image: '', company: '株式会社ヘプタゴン', name: '三浦 一樹さん' },
+  { image: '', company: '株式会社DIGITALJET', name: '古里 武士さん' },
+  { image: '', company: '株式会社Fusic', name: '清家 史郎さん' },
+  { image: '/speakers/b5f641fdf20fc10651b5c8797b6e6461.png', company: '株式会社Helpfeel', name: '秋山さん' },
+  { image: '/speakers/1746562415085.jpg', company: 'Studio, Inc', name: '八木さん' },
+  { image: '/speakers/12a26cb6aa9617aed86cbb8ea40e5f2b.jpg', company: 'アトモフ株式会社', name: '中野 恭兵さん' },
+  { image: '/speakers/3d64468036802b636c3b2b10623099c3.jpg', company: '株式会社 TBSテレビ', name: '亀田 遼さん' },
+  { image: '/speakers/a3de51c09d6885bd730934cba9e69eab.jpg', company: '株式会社favy', name: '森さん' },
+  { image: '/speakers/e279e71ea43f36f207e3508ee66b83ba.jpg', company: '株式会社DIGITALJET', name: '真崎 克宏さん' },
+  { image: '/speakers/DSC_5282.JPG', company: 'Revtrona', name: '岡本 秀高' },
 ];
 
 export default function Speakers() {
@@ -22,15 +27,26 @@ export default function Speakers() {
           {speakers.map((speaker, index) => (
             <div key={index} className="speaker-item">
               <div className="speaker-image-wrapper">
-                <Image
-                  src={speaker.image}
-                  alt={speaker.name}
-                  width={300}
-                  height={300}
-                  className="speaker-image"
-                />
+                {speaker.image ? (
+                  <Image
+                    src={speaker.image}
+                    alt={speaker.company ? `${speaker.company} ${speaker.name}` : speaker.name}
+                    width={300}
+                    height={300}
+                    className="speaker-image"
+                  />
+                ) : (
+                  <div className="speaker-placeholder">
+                    <span className="speaker-placeholder-text">画像準備中</span>
+                  </div>
+                )}
               </div>
-              <div className="speaker-name">{speaker.name}</div>
+              <div className="speaker-info">
+                {speaker.company && (
+                  <div className="speaker-company">{speaker.company}</div>
+                )}
+                <div className="speaker-name">{speaker.name}</div>
+              </div>
             </div>
           ))}
         </div>
